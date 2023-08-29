a3b969cd37a5ff19916ac4ee2591903f14526120
Grantmart9 <grantmarthinus@gmail.com>
2023-08-29 20:17:04 +0200

    s

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -55,21 +55,6 @@ export const Products = () => {
     setOpen(!open);
   };
 
-  if (loading) {
-    return (
-      <div
-        style={{ color: pageHeading }}
-        className="flex justify-center align-center"
-      >
-        <div className="grid grid-rows-2 gap-6">
-          <div className="flex align-center my-20">
-            <img height={80} width={80} src={Loading} />
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   const handleAmount = (event) => {
     setAmount(event.target.value);
     console.log(amount);
@@ -106,104 +91,116 @@ export const Products = () => {
 
   return (
     <div>
-      <div
-        style={{ color: pageHeading }}
-        className="text-xl flex align-center justify-center mb-2 p-2 w-full"
-      >
-        Products
-      </div>
-      <div className="grid grid-cols-3 gap-2">
-        <Button sx={{ backgroundColor: layoutColor, color: buttonColor }}>
-          <div style={{ fontFamily: fontType }}>fragrances</div>
-        </Button>
-        <Button sx={{ backgroundColor: layoutColor, color: buttonColor }}>
-          <div style={{ fontFamily: fontType }}>skincare</div>
-        </Button>
-        <Button sx={{ backgroundColor: layoutColor, color: buttonColor }}>
-          <div style={{ fontFamily: fontType }}>makeup</div>
-        </Button>
-      </div>
-      <div className="grid grid-cols-4 gap-2">
-        {menu.map((product, i) => (
-          <div key={i} className="p-2">
-            <Button onClick={() => handleClick({ product })}>
-              {product.image}
-            </Button>
-            <div className="text-center mt-1">{product.description}</div>
-            <div className="text-center">{product.brand}</div>
-            <div className="text-center">{product.price}</div>
+      {!loading ? (
+        <div>
+          {" "}
+          <div
+            style={{ color: pageHeading }}
+            className="text-xl flex align-center justify-center mb-2 p-2 w-full"
+          >
+            Products
           </div>
-        ))}
-      </div>
-      <Dialog
-        open={open}
-        fullScreen={true}
-        keepMounted
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogContent>
-          <div className="flex align-center justify-end">
-            <Button
-              sx={{ backgroundColor: layoutColor, color: buttonColor }}
-              onClick={() => setOpen(!open)}
-            >
-              close
+          <div className="grid grid-cols-3 gap-2">
+            <Button sx={{ backgroundColor: layoutColor, color: buttonColor }}>
+              <div style={{ fontFamily: fontType }}>fragrances</div>
+            </Button>
+            <Button sx={{ backgroundColor: layoutColor, color: buttonColor }}>
+              <div style={{ fontFamily: fontType }}>skincare</div>
+            </Button>
+            <Button sx={{ backgroundColor: layoutColor, color: buttonColor }}>
+              <div style={{ fontFamily: fontType }}>makeup</div>
             </Button>
           </div>
-          <div className="grid grid-rows-5 gap-1">
-            <div className="transition ease-in duration-3000 mx-auto my-auto">
-              {item.image}
-            </div>
-            <div className="flex text-sm font-bold align-center justify-center">
-              {item.name}
-            </div>
-            <div className="flex text-sm font-bold align-center justify-center">
-              {item.description}
-            </div>
-            <div className="flex text-sm align-center justify-center">
-              {item.brand}
-            </div>
-            <div className="flex text-lg align-center justify-center">
-              {item.price}
-            </div>
+          <div className="grid grid-cols-4 gap-2">
+            {menu.map((product, i) => (
+              <div key={i} className="p-2">
+                <Button onClick={() => handleClick({ product })}>
+                  {product.image}
+                </Button>
+                <div className="text-center mt-1">{product.description}</div>
+                <div className="text-center">{product.brand}</div>
+                <div className="text-center">{product.price}</div>
+              </div>
+            ))}
           </div>
-        </DialogContent>
-        <div className="grid grid-rows-2 gap-1">
-          <div className="flex align-center justify-center">
-            <div className="grid grid-rows-2">
-              <div className="text-xs">Volume</div>
-              <Select value={amount} onChange={handleVolume}>
-                <MenuItem value={10}>10ml</MenuItem>
-                <MenuItem value={30}>30ml</MenuItem>
-                <MenuItem value={100}>100ml</MenuItem>
-              </Select>
+          <Dialog
+            open={open}
+            fullScreen={true}
+            keepMounted
+            aria-describedby="alert-dialog-slide-description"
+          >
+            <DialogContent>
+              <div className="flex align-center justify-end">
+                <Button
+                  sx={{ backgroundColor: layoutColor, color: buttonColor }}
+                  onClick={() => setOpen(!open)}
+                >
+                  close
+                </Button>
+              </div>
+              <div className="grid grid-rows-5 gap-1">
+                <div className="transition ease-in duration-3000 mx-auto my-auto">
+                  {item.image}
+                </div>
+                <div className="flex text-sm font-bold align-center justify-center">
+                  {item.name}
+                </div>
+                <div className="flex text-sm font-bold align-center justify-center">
+                  {item.description}
+                </div>
+                <div className="flex text-sm align-center justify-center">
+                  {item.brand}
+                </div>
+                <div className="flex text-lg align-center justify-center">
+                  {item.price}
+                </div>
+              </div>
+            </DialogContent>
+            <div className="grid grid-rows-2 gap-1">
+              <div className="flex align-center justify-center">
+                <div className="grid grid-rows-2">
+                  <div className="text-xs">Volume</div>
+                  <Select value={amount} onChange={handleVolume}>
+                    <MenuItem value={10}>10ml</MenuItem>
+                    <MenuItem value={30}>30ml</MenuItem>
+                    <MenuItem value={100}>100ml</MenuItem>
+                  </Select>
+                </div>
+                <div className="grid grid-rows-2 ml-2">
+                  <div className="text-xs">Quantity</div>
+                  <Select value={volume} onChange={handleAmount}>
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                  </Select>
+                </div>
+              </div>
+              <div className="flex align-center justify-center mt-2">
+                <Button
+                  onClick={handleAdd}
+                  size="medium"
+                  sx={{
+                    backgroundColor: layoutColor,
+                    color: buttonColor,
+                    height: "50px",
+                  }}
+                >
+                  Add to Cart
+                </Button>
+              </div>
             </div>
-            <div className="grid grid-rows-2 ml-2">
-              <div className="text-xs">Quantity</div>
-              <Select value={volume} onChange={handleAmount}>
-                <MenuItem value={1}>1</MenuItem>
-                <MenuItem value={2}>2</MenuItem>
-                <MenuItem value={3}>3</MenuItem>
-                <MenuItem value={4}>4</MenuItem>
-                <MenuItem value={5}>5</MenuItem>
-              </Select>
-            </div>
-          </div>
-          <div className="flex align-center justify-center mt-2">
-            <Button
-              onClick={handleAdd}
-              size="medium"
-              sx={{
-                backgroundColor: layoutColor,
-                color: buttonColor,
-                height: "50px",
-              }}
-            >
-              Add to Cart
-            </Button>
-          </div>
+          </Dialog>
         </div>
-      </Dialog>
+      ) : (
+        <div
+          style={{ color: pageHeading }}
+          className="h-screen flex items-center justify-center"
+        >
+          <img width={80} height={80} src={Loading} />
+        </div>
+      )}
     </div>
   );
 };
