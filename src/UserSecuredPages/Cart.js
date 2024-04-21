2ec9f9abdd876f26234e70b4cbd250acd7776bfa
Grantmart9 <grantmarthinus@gmail.com>
2024-04-21 13:46:13 +0200

    f

--- src/UserSecuredPages/Cart.js
+++ src/UserSecuredPages/Cart.js
@@ -25,15 +25,81 @@ import {
 } from "react-router-dom";
 const cookies = new Cookies();
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+const UserCart = ({ data, handleAdd, handleSub }) => {
+  return (
+    <div
+      style={{ color: pageHeading, marginTop: "1pt" }}
+      className="text-xl flex align-center justify-center mb-2 p-2 w-full"
+    >
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ font: "bold" }} align="left">
+                Product
+              </TableCell>
+
+              <TableCell align="left">Quantity</TableCell>
+              <TableCell align="left">Delivery time (days)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{
+                  "&:last-child td, &:last-child th": { border: 0 },
+                }}
+              >
+                <TableCell align="left">{row.product_description}</TableCell>
+
+                <TableCell align="left">
+                  <Button className="ml-2" onClick={handleAdd}>
+                    &darr;
+                  </Button>
+                  {row.qty}
+                  <Button className="mr-2" onClick={handleSub}>
+                    &uarr;
+                  </Button>
+                </TableCell>
+                <TableCell align="left">{row.delivery_time}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
 
 export const Cart = () => {
   const size = Size();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [data, setData] = useState();
+
+  const handleAdd = () => {
+    axios
+      .post("http://" + API_IP + "/add_to_cart", {
+        product_code: "GX1001",
+        amount: 1,
+        token: cookies.get("Token"),
+        cart_id: 2,
+      })
+      .then((res) => setData(res.data) + setLoading(false))
+      .catch(function (error) {});
+  };
+
+  const handleSub = () => {
+    axios
+      .post("http://" + API_IP + "/remove_from_cart", {
+        product_code: "GX1001",
+        token: cookies.get("Token"),
+        cart_id: 2,
+      })
+      .then((res) => setData(res.data) + setLoading(false))
+      .catch(function (error) {});
+  };
 
   useEffect(() => {
     var token = cookies.get("Token");
@@ -49,7 +115,7 @@ export const Cart = () => {
         setError(true);
         setLoading(false);
       });
-  }, []);
+  }, [loading]);
 
   return (
     <div>
@@ -62,7 +128,6 @@ export const Cart = () => {
         />
       ) : (
         <div>
-          {" "}
           {!loading ? (
             <div>
               {size == "MD" || size == "SM" || size == "XS" ? (
@@ -70,67 +135,31 @@ export const Cart = () => {
                   style={{ color: pageHeading, marginTop: "15%" }}
                   className="text-xl flex align-center justify-center mb-2 p-2 w-full"
                 >
-                  {data.map((item) => (
-                    <div className="grid grid-rows-4 gap-2 mb-4 shadow-md p-3">
-                      <div>Brand: {item.brand}</div>
-                      <div>Description: {item.product_description}</div>
-                      <div>Qty: {item.qty}</div>
-                      <div>Delivery time: {item.delivery_time} days</div>
-                    </div>
-                  ))}
+                  <UserCart
+                    data={data}
+                    handleSub={handleAdd}
+                    handleAdd={handleSub}
+                  />
                 </div>
               ) : (
-                <div
-                  style={{ color: pageHeading, marginTop: "1pt" }}
-                  className="text-xl flex align-center justify-center mb-2 p-2 w-full"
-                >
-                  <TableContainer component={Paper}>
-                    <Table>
-                      <TableHead>
-                        <TableRow>
-                          <TableCell align="left">Product</TableCell>
-                          <TableCell align="left">Brand</TableCell>
-                          <TableCell align="left">Quantity</TableCell>
-                          <TableCell align="left">
-                            Delivery time (days)
-                          </TableCell>
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {data.map((row) => (
-                          <TableRow
-                            key={row.name}
-                            sx={{
-                              "&:last-child td, &:last-child th": { border: 0 },
-                            }}
-                          >
-                            <TableCell align="left">
-                              {row.product_description}
-                            </TableCell>
-                            <TableCell align="left">{row.brand}</TableCell>
-                            <TableCell align="left">{row.qty}</TableCell>
-                            <TableCell align="left">
-                              {row.delivery_time}
-                            </TableCell>
-                          </TableRow>
-                        ))}
-                      </TableBody>
-                    </Table>
-                  </TableContainer>
-                </div>
+                <UserCart
+                  data={data}
+                  handleSub={handleAdd}
+                  handleAdd={handleSub}
+                />
               )}
-              <Button
-                size="medium"
-                sx={{
-                  backgroundColor: layoutColor,
-                  color: buttonColor,
-                  height: "50px",
-                  alignSelf: "center",
-                  justifySelf: "center",
-                }}
-              >
-                Checkout
-              </Button>
+              <div className="flex align-center justify-center mt-4">
+                <Button
+                  size="medium"
+                  sx={{
+                    backgroundColor: layoutColor,
+                    color: buttonColor,
+                    height: "50px",
+                  }}
+                >
+                  Checkout
+                </Button>
+              </div>
             </div>
           ) : (
             <div>Loading...</div>
